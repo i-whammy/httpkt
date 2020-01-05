defb65e9e2be715aa90a315d427b53f4b66491b6
i-whammy <yas.iwami@gmail.com>
2020-01-05 17:53:45 +0900

    removed unused test

--- simple-clojure-http-server/test/simple_clojure_http_server/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns simple-clojure-http-server.core-test
-  (:require [clojure.test :refer :all]
-            [simple-clojure-http-server.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
